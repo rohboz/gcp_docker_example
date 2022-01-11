13e4618776d22e1319338a4bf550d7eb8d4eaa8a
rohboz <33161645+rohboz@users.noreply.github.com>
2022-01-11 15:18:12 +0100

    updated main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
 
     # Predict the value of the digit on the test subset
     predicted = clf.predict(X_test)
-
+    print("Hello World!")
     print(
         f"Classification report for classifier {clf}:\n"
         f"{metrics.classification_report(y_test, predicted)}\n"
